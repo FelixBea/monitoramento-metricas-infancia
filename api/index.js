dc3faf771f0e3dd2523fd57d691f2c83159d95ee
Beatriz Felix <beatriz-felix@pop-os.localdomain>
2026-04-22 11:55:56 -0300

    chore: initial commit

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Monitoramento de Métricas da Infancia API');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
